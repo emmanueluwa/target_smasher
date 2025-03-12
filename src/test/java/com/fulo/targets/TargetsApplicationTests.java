99c7106df2ebe1847a311436b3a176aad934e974
emmanuel <oemmanuel.uwadiae@gmail.com>
2025-03-12 05:40:23 +0000

    initial commit

---
+++ src/test/java/com/fulo/targets/TargetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fulo.targets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TargetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
